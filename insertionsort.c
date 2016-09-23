31bea8c255d7051a36997937830fac7b13e53365
krishnanm86 <krishna.nm86@gmail.com>
2016-09-23 18:50:50 +0200

    CHanges

--- insertionsort.c
+++ insertionsort.c
@@ -1,58 +1,58 @@
-#include <stdlib.h>
-#include <string.h>
+/*
+ Petar 'PetarV' Velickovic
+ Algorithm: Insertion Sort
+*/
+
 #include <stdio.h>
-#include "gmp.h"
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <complex>
+#define MAX_N 5001
+using namespace std;
+typedef long long lld;
 
-void insertion_sort(mpz_t *arr, int len) {
-    /* sorts arr in place using insertion sort */
+int n;
+mpz_t *niz;
 
-    int i, j;
-    mpz_t key;
-    mpz_init(key);
+//Insertion sort algoritam za sortiranje niza
+//Slozenost: O(n^2)
 
-    for (i = 1; i < len; ++i) {
-        mpz_set(key, arr[i]);
-        j = i-1;
-
-        while (j >= 0 && mpz_cmp(arr[j], key) > 0) {
-            mpz_set(arr[j+1], arr[j]);
-            --j;
+inline void insertionSort()
+{
+    for (int i=1;i<n;i++)
+    {
+        int j = i - 1;
+        mpz_t tmp;
+        mpz_init_set(tmp, niz[i]);
+        while (j >= 0 && mpz_cmp(niz[j], tmp) > 0) 
+        {
+          mpz_set(niz[j+1], niz[j]);
+          j--;
         }
-
-        mpz_set(arr[j+1], key);
+        mpz_set(niz[j+1], tmp);
     }
 }
 
-int main(int argc, char **argv) {
-    
-    mpz_t *arr;
-    arr = malloc(8* sizeof(mpz_t));
-
-    mpz_init_set_ui(arr[0], 4);
-    
-    mpz_init_set_ui(arr[1], 5);
-    
-    mpz_init_set_ui(arr[2], 20);
-    
-    mpz_init_set_ui(arr[3], 6);
-    
-    mpz_init_set_ui(arr[4], 2);
-    
-    mpz_init_set_ui(arr[5], 3);
-    
-    mpz_init_set_ui(arr[6], 12);
-    
-    mpz_init_set_ui(arr[7], 1);
-    
-
-    insertion_sort(arr, 8);
-
-    int i;
-    for (i = 0; i < 8; ++i) {
-        gmp_printf(" %Zd ", arr[i]);
-    }
-
+int main()
+{
+    n = 5;
+    niz =  malloc(MAX_N* sizeof(mpz_t));
+    mpz_init_set_ui(niz[0], 4);
+    mpz_init_set_ui(niz[1], 2);
+    mpz_init_set_ui(niz[2], 5);
+    mpz_init_set_ui(niz[3], 1);
+    mpz_init_set_ui(niz[4], 3);
+    insertionSort();
+    for (int i=0;i<n;i++) gmp_printf("%Zd ",niz[i]);
     printf("\n");
-
     return 0;
 }
